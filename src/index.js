211d027308ee1d1f3622b1287cea47c7a646ec23
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-14 00:49:23 +0300

    feat: add a solution for 13 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let matchingOpeningBracket, ch
+    let stack = [];
+    let bracketsConfigArray = [].concat(...bracketsConfig);
+    let openingBrackets = [];
+    let closingBrackets = [];
+    if (str.length <= 1){
+        return false
+    } else
+    {
+        for (let i=0; i < bracketsConfigArray.length; i++ ){
+            if (i % 2 === 0 ){
+                openingBrackets.push(bracketsConfigArray[i]);
+            } else
+            {
+                closingBrackets.push(bracketsConfigArray[i]);
+            };
+        };
+    };
+
+    for (let i = 0; i < str.length; i++) {
+        ch = str[i]
+
+        if (closingBrackets.indexOf(ch) > -1) {
+            matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+            if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+                return false
+            }
+        } else {
+            stack.push(ch)
+        }
+    }
+
+    return (stack.length == 0)
+
 }
