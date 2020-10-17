4b86cc3b31e4958b7835fda3c800d994cccb8d6d
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-17 22:21:09 +0300

    feat: add a solution for 14 tests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-    let matchingOpeningBracket, ch
+    let matchingOpeningBracket, ch;
     let stack = [];
     let bracketsConfigArray = [].concat(...bracketsConfig);
     let openingBrackets = [];
     let closingBrackets = [];
     if (str.length <= 1){
-        return false
+        return false;
     } else
     {
         for (let i=0; i < bracketsConfigArray.length; i++ ){
@@ -14,23 +14,49 @@ module.exports = function check(str, bracketsConfig) {
             } else
             {
                 closingBrackets.push(bracketsConfigArray[i]);
-            };
-        };
-    };
-
-    for (let i = 0; i < str.length; i++) {
-        ch = str[i]
-
-        if (closingBrackets.indexOf(ch) > -1) {
-            matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
-            if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
-                return false
             }
-        } else {
-            stack.push(ch)
         }
     }
 
-    return (stack.length == 0)
+    for (let i = 0; i < str.length; i++) {
+        ch = str[i];
 
-}
+        if (closingBrackets.indexOf(ch) > -1) {
+            matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)];
+            if ((stack.length == 0 || (stack.pop() != matchingOpeningBracket)) && openingBrackets[closingBrackets.indexOf(ch)] !== closingBrackets[closingBrackets.indexOf(ch)]) {
+                return false;
+            }
+        } else {
+            stack.push(ch);
+        }
+    }
+
+    return (stack.length == 0);
+};
+
+
+
+// };
+
+
+// let bracketsConfigString = [].concat(...bracketsConfig).join('');
+// // console.log(bracketsConfigArray);
+// // let brackets = "[]{}()<>"
+// let stack = []
+//
+// for(let bracket of str) {
+//     let bracketsConfigStringIndex = bracketsConfigString.indexOf(bracket)
+//
+//     if (bracketsConfigStringIndex === -1){
+//         continue
+//     }
+//
+//     if(bracketsConfigStringIndex % 2 === 0) {
+//         stack.push(bracketsConfigStringIndex + 1)
+//     } else {
+//         if(stack.pop() !== bracketsConfigStringIndex) {
+//             return false;
+//         }
+//     }
+// }
+// return stack.length === 0
