2333d943517ee7970ed6899844b5e126c5763e3b
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-18 01:19:06 +0300

    feat: add a solution for task

--- src/index.js
+++ src/index.js
@@ -17,46 +17,71 @@ module.exports = function check(str, bracketsConfig) {
             }
         }
     }
-
     for (let i = 0; i < str.length; i++) {
         ch = str[i];
 
         if (closingBrackets.indexOf(ch) > -1) {
             matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)];
-            if ((stack.length == 0 || (stack.pop() != matchingOpeningBracket)) && openingBrackets[closingBrackets.indexOf(ch)] !== closingBrackets[closingBrackets.indexOf(ch)]) {
-                return false;
+            if ((openingBrackets[closingBrackets.indexOf(ch)] === closingBrackets[closingBrackets.indexOf(ch)]) && (stack[stack.length-1] != matchingOpeningBracket) &&  (stack.indexOf(ch) === -1))  {
+                stack.push(ch);
+
             }
-        } else {
+            else {
+                if ((stack.length == 0 || (stack.pop() != matchingOpeningBracket)) && openingBrackets[closingBrackets.indexOf(ch)] !== closingBrackets[closingBrackets.indexOf(ch)]) {
+                    return false;
+                }
+            }
+        }
+        else {
             stack.push(ch);
         }
     }
-
     return (stack.length == 0);
 };
 
-
-
-// };
-
-
-// let bracketsConfigString = [].concat(...bracketsConfig).join('');
-// // console.log(bracketsConfigArray);
-// // let brackets = "[]{}()<>"
-// let stack = []
+// let matchingOpeningBracket, ch;
+// let stack = [];
+// let bracketsConfigArray = [].concat(...bracketsConfig);
+// let openingBrackets = [];
+// let closingBrackets = [];
+// if (str.length <= 1){
+//     return false;
+// } else
+// {
+//     for (let i=0; i < bracketsConfigArray.length; i++ ){
+//         if (i % 2 === 0 ){
+//             openingBrackets.push(bracketsConfigArray[i]);
+//         } else
+//         {
 //
-// for(let bracket of str) {
-//     let bracketsConfigStringIndex = bracketsConfigString.indexOf(bracket)
-//
-//     if (bracketsConfigStringIndex === -1){
-//         continue
-//     }
-//
-//     if(bracketsConfigStringIndex % 2 === 0) {
-//         stack.push(bracketsConfigStringIndex + 1)
-//     } else {
-//         if(stack.pop() !== bracketsConfigStringIndex) {
-//             return false;
+//             closingBrackets.push(bracketsConfigArray[i]);
 //         }
 //     }
+//
 // }
-// return stack.length === 0
+// for (let i = 0; i < str.length; i++) {
+//     ch = str[i];
+//     console.log(stack, "loop")
+//     if (closingBrackets.indexOf(ch) > -1) {
+//         matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)];
+//         if ((openingBrackets[closingBrackets.indexOf(ch)] === closingBrackets[closingBrackets.indexOf(ch)]) && (stack[stack.length-1] != matchingOpeningBracket) &&  (stack.indexOf(ch) === -1))  {
+//             stack.push(ch);
+//             //  console.log(stack,"wd")
+//             //  console.log(ch)
+//             //  console.log(stack+'1');
+//         }
+//         else {
+//             if ((stack.length == 0 || (stack.pop() != matchingOpeningBracket)) && openingBrackets[closingBrackets.indexOf(ch)] !== closingBrackets[closingBrackets.indexOf(ch)]) {
+//                 // console.log(stack)
+//                 return false;
+//             }
+//         }
+//
+//     }
+//     else {
+//         stack.push(ch);
+//         // console.log(stack)
+//     }
+// }
+// return (stack.length == 0);
+// };
